f4ad154804cfbd6a676754a3a288d6eb9eac9861
Alsu <evergreensun@yandex.ru>
2022-04-21 20:09:07 +0300

    lil change

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,36 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        node: true
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
     },
-    "extends": [
+    extends: [
         "eslint:recommended",
         "plugin:react/recommended",
-        'plugin:react-hooks/recommended'
+        "plugin:react-hooks/recommended",
+        "plugin:@typesscript-eslint/recommended",
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:import/typescript',
+        'plugin:prettier/recommended', 
+        'prettier',
+        'prettier/react'
+
     ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
         },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+        ecmaVersion: "latest",
+        sourceType: "module",
     },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
+    plugins: ["react", "@typesscript-eslint", 'prettier'],
+    rules: {
+        "react-hooks/rules-of-hooks": ["error"],
+        "react-hooks/exhaustive-deps": "warn",
+        "@typescript-eslint/array-type": "off",
+        "prettier/prettier": "error",
+        '@typescript-eslint/no-explicit-any': 'warn',
+
     }
 }
